62407b10b7175479f695aa7b1d4fdee8bd91719e
svetlana.isakova <svetlana.isakova@jetbrains.com>
2026-06-10 13:49:22 +0200

    Copy IDE plugin template project into a module

--- ide-plugin/build.gradle.kts
+++ ide-plugin/build.gradle.kts
@@ -1,18 +1,17 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 plugins {
-    kotlin("jvm") version "2.4.0"
-}
-
-group = "org.jetbrains.koog.graph.checker"
-version = "0.1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.intellij.platform")
+    id("org.jetbrains.changelog")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
+    testImplementation("junit:junit:4.13.2")
 
-tasks.test {
-    useJUnitPlatform()
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        intellijIdea("2025.2.6.2")
+        testFramework(TestFrameworkType.Platform)
+    }
 }

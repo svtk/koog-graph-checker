fffd3b38809670f596abb13ffabca332e2be6907
svetlana.isakova <svetlana.isakova@jetbrains.com>
2026-06-10 14:36:07 +0200

    Refactor Gradle settings to consolidate IDE plugin configuration into the root project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,27 @@
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
+
 pluginManagement {
+    plugins {
+        id("org.jetbrains.changelog") version "2.5.0"
+    }
     repositories {
         mavenCentral()
         gradlePluginPortal()
     }
-
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
+}
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        intellijPlatform {
+            defaultRepositories()
+        }
     }
 }
 
@@ -16,5 +29,4 @@ rootProject.name = "koog-graph-checker"
 
 include("compiler-plugin")
 include("gradle-plugin")
-
 include("ide-plugin")
